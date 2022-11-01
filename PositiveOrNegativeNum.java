0fe38348387f18188736ab91c0c8ff7f3ad09027
VivekGujale <vdgujale007@.com>
2022-11-01 23:38:27 +0530

    added Day18 java file

---
+++ PositiveOrNegativeNum.java
@@ -0,0 +1,11 @@
+//Write a program to find number is positive or negative.
+
+public class PositiveOrNegativeNum {
+    public static void main(String[] args) {
+        int a = 5;
+        if (a > 0)
+            System.out.println("Positive Number");
+        else
+            System.out.println("Negative Number");
+    }
+}
